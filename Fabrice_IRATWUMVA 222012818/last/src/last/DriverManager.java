d67b1eaf596c703869c409874646806dd34c7ccf
Fabira132 <151012313+Fabira132@users.noreply.github.com>
2024-02-19 10:46:57 +0200

    Add files via upload

---
+++ Fabrice_IRATWUMVA 222012818/last/src/last/DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package last;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author user1
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhostapartment, String root, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
